fe0201002697d1d5cb4da90eecd66ba6510d3165
Sergio Ruiz <sergioruizrock@gmail.com>
2024-01-23 12:03:48 -0300

    se realizo el ejercicio 3

--- src/components/Saludo.jsx
+++ src/components/Saludo.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 const Saludo = (props) => {
+    const [txt,text]=useState();
+
     return (
         <div>
-            <h1 className="display-3">Hello {props.data}!</h1>
+            <h1 className="display-3">Hello {props.data} {txt}!</h1>
+            <button className="btn btn-primary" onClick={()=>{text("(from changed State)")}}>Click!</button>
         </div>
     );
 };
